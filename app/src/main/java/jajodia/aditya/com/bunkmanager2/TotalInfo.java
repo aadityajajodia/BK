911fdcf7e4be8c455f88c579ed4d83d3e1802c7e
kinglesnar <singh.kunal112@gmail.com>
2016-12-11 18:16:37 +0530

    relative layout dynamic problem solved

--- app/src/main/java/jajodia/aditya/com/bunkmanager2/TotalInfo.java
+++ app/src/main/java/jajodia/aditya/com/bunkmanager2/TotalInfo.java
@@ -16,7 +16,7 @@ public class TotalInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(li);
+        setContentView(R.layout.activity_total_info);
 
         int size = MainActivity.getsize();
         Log.d(TAG,"size"+size);// no. of subjects
@@ -31,31 +31,31 @@ public class TotalInfo extends AppCompatActivity {
                 j=i+1;
             }
         }
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        li = (RelativeLayout)findViewById(R.id.activity_total_info);
+        RelativeLayout.LayoutParams params[] = new RelativeLayout.LayoutParams[size];
         Button btn[] = new Button[size];
         for(int i=0;i<size;i++){
             btn[i] = new Button(this);
-            //Log.d(TAG,"size :"+size);
+            btn[i].setId((i+1)*4);
+            params[i] = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+            if(i%3==0&&i>0){
+               Log.d(TAG,"came "+" "+i);
+                params[i].addRule(RelativeLayout.BELOW,btn[i-3].getId());
+
+            }
+        else if(i>0&&i>3){
+                params[i].addRule(RelativeLayout.RIGHT_OF,btn[i-1].getId());
+                params[i].addRule(RelativeLayout.BELOW,btn[i-3].getId());
+            }
+            else if(i>0){
+                params[i].addRule(RelativeLayout.RIGHT_OF,btn[i-1].getId());
+            }
+            else{
+                params[i].addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            }
             btn[i].setText(subjects[i]);
-            btn[i].setId(i);
-           /* if(i%3==0&&i>0){
-                Log.d(TAG,"came in loop 1");
-                params.addRule(RelativeLayout.BELOW,btn[i-3].getId());
-            }
-             else if(i>0){
-                Log.d(TAG,"came in loop 2");
-                 params.addRule(RelativeLayout.RIGHT_OF,btn[i-1].getId());
-
-             }*/
-            if(i>0){
-                params.addRule(RelativeLayout.BELOW,btn[i-1].getId());
-            }
-            btn[i].setLayoutParams(params);
-            li.addView(btn[i],params);
-           setContentView(li);
-
-            //btn[i].setLayoutParams(params);
+            btn[i].setLayoutParams(params[i]);
+            li.addView(btn[i],params[i]);
         }
     }
 }
